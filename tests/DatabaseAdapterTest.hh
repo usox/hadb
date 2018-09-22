a45d7284830c0ba2a7f2b1ffe8c84d363f3047e4
Daniel Jakob <wolverine@usox.org>
2018-09-22 07:52:14 +0200

    Bump hhvm to 3.28; update tests

--- tests/DatabaseAdapterTest.hh
+++ tests/DatabaseAdapterTest.hh
@@ -2,28 +2,29 @@
 namespace Usox\HaDb;
 
 use function Facebook\FBExpect\expect;
+use function Usox\HackMock\{mock, prospect};
 
-class DatabaseAdapterTest extends \PHPUnit_Framework_TestCase {
+class DatabaseAdapterTest extends \Usox\HackMock\HackMock {
 
-    private ?\PDO $pdo;
+    <<__LateInit>>
+    private \PDO $pdo;
 
-    private ?DatabaseAdapter $adapter;
+    <<__LateInit>>
+    private DatabaseAdapter $adapter;
 
-    private ?DatabaseConfigInterface $config;
+    <<__LateInit>>
+    private DatabaseConfigInterface $config;
 
     <<__Override>>
-    public function setUp(): void {
-        $this->config = $this->createMock(DatabaseConfigInterface::class);
-        // UNSAFE
-        $this->pdo = \Mockery::mock(\PDO::class);
+    public async function beforeEachTestAsync(): Awaitable<void> {
+        $this->config = mock(DatabaseConfigInterface::class);
+        $this->pdo = mock(\PDO::class);
 
         $this->adapter = new DatabaseAdapter(
             function (DatabaseConfigInterface $config): \PDO {
-                //UNSAFE
-                $this->pdo
-                    ?->shouldReceive('setAttribute')
-                    ?->with(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION)
-                    ?->once();
+                prospect($this->pdo, 'setAttribute')
+                    ->with(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION)
+                    ->once();
                 return $this->pdo;
             },
             $this->config
@@ -32,13 +33,20 @@ class DatabaseAdapterTest extends \PHPUnit_Framework_TestCase {
 
     public function testGetConnectionReturnsConnection(): void {
         expect(
-            $this->adapter?->getConnection()
+            $this->adapter->getConnection()
         )
         ->toBeSame($this->pdo);
     }
 
     public function testGetQueryCountIncrementsAfterQuery(): void {
-        expect($this->adapter?->getQueryCount())->toBeSame(0);
+        prospect($this->pdo, 'query')
+			->with('foobar')
+            ->once()
+            ->andReturn(mock(\PDOStatement::class));
+
+        $this->adapter->query('foobar');
+
+        expect($this->adapter->getQueryCount())->toBeSame(1);
     }
 
     public function testGetConnectionThrowExceptionOnInitializationError(): void {
@@ -48,7 +56,7 @@ class DatabaseAdapterTest extends \PHPUnit_Framework_TestCase {
             (DatabaseConfigInterface $config): \PDO ==> {
                 throw new \PDOException($error_message);
             },
-            $this->createMock(DatabaseConfigInterface::class)
+            mock(DatabaseConfigInterface::class)
         );
         expect(
             () ==> $adapter->getConnection()
